d800f1f06c8480297f79bdcea0648c4ce7ad0fe1
viperovm <53254495+viperovm@users.noreply.github.com>
2020-11-08 21:34:11 +0100

    credits

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,10 @@
     {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/jquery.modal.min.css') }}"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+    <link rel="stylesheet" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/fontawesome.min.js" type="text/javascript" charset="utf-8"></script>
 @yield('stylesheets')
 
 @if(config('app.env') == 'production')

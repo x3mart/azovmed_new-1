17d05e1dabbc4591934cf4880773e1bd911d9219
viperovm <53254495+viperovm@users.noreply.github.com>
2021-01-21 10:31:57 +0100

    Update bid.blade.php
    Changed places

--- resources/views/form/bid.blade.php
+++ resources/views/form/bid.blade.php
@@ -3,5 +3,5 @@
     <input type="hidden" name="referrer" value="{{ Request::url() }}">
     <input type="text" id="indexEditbox1" style="position:absolute;left:27px;top:16px;width:249px;height:43px;line-height:43px;z-index:31;" name="name" required placeholder="Ваше имя">
     <input type="email" id="indexEditbox2" style="position:absolute;left:28px;top:68px;width:249px;height:43px;line-height:43px;z-index:30;" name="email" required placeholder="Ваш e-mail">
-    <input type="submit" id="indexButton1" value="Заказать" onclick="e.preventDefault(); document.getElementById('indexForm1').submit(); this.setAttribute('disabled', true);" style="position:absolute;left:27px;top:121px;width:249px;height:50px;z-index:29;">
+    <input type="submit" id="indexButton1" value="Заказать" onclick="e.preventDefault(); this.setAttribute('disabled', true); document.getElementById('indexForm1').submit();" style="position:absolute;left:27px;top:121px;width:249px;height:50px;z-index:29;">
 </form>
